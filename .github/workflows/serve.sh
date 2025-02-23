400a5458fd53433b9ec563e35a4588f6938ac373
Micah Turner <turner.micah@gmail.com>
2025-02-22 18:28:36 -0700

    ci going maybe

--- .github/workflows/serve.sh
+++ .github/workflows/serve.sh
@@ -4,7 +4,7 @@ set -e
 
 rm -rf makelove-build
 makelove lovejs
-unzip -o "makelove-build/lovejs/game-name-goes-here-lovejs" -d makelove-build/html/
-echo "http://localhost:8000/makelove-build/html/game-name-goes-here/"
+unzip -o "makelove-build/lovejs/gdi-control-the-environment-lovejs" -d makelove-build/html/
+echo "http://localhost:8000/makelove-build/html/gdi-control-the-environment/"
 python3 -m http.server
 # ruby -run -e httpd . -p 8000
